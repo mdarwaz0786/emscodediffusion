7cbaa5dee9bb1afa854ae5eea84854d3d049ade5
Md Arwaz <mdarwaznew2017@gmail.com>
2025-01-29 18:59:19 +0530

    fix bug

--- src/Components/Main/SalarySlip/SalarySlip.js
+++ src/Components/Main/SalarySlip/SalarySlip.js
@@ -51,304 +51,305 @@ const SalarySlip = () => {
       return;
     };
 
-    const html =
-      `< !DOCTYPE html>
-        <html lang="en">
+    const html = `
+<!DOCTYPE html>
+<html lang="en">
 
-          <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Salary Slip</title>
-            <style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Salary Slip</title>
+  <style>
+ * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  body {
-    font-family: Arial, sans-serif;
-    background-color: #f4f4f4;
-  }
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+}
 
-  .page-wrapper {
-    margin: 20px;
-  }
+.page-wrapper {
+ margin: 20px;
+}
 
-  .content {
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 5px;
-  }
+.content {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+}
 
-  .salary-slip {
-    padding: 20px;
-    background-color: white;
-  }
+.salary-slip {
+  padding: 20px;
+  background-color: white;
+}
 
-  .logo-section {
-    margin-bottom: 30px;
-  }
+.logo-section {
+  margin-bottom: 30px;
+}
 
-  .logo {
-    width: 150px;
-    height: 30px;
-    object-fit: contain;
-  }
+.logo {
+  width: 150px;
+  height: 30px;
+  object-fit: contain;
+}
 
-  .company-details {
-    margin-bottom: 20px;
-  }
+.company-details {
+  margin-bottom: 20px;
+}
 
-  .company-name {
-    font-weight: 600;
-    font-size: 20px;
-    margin-bottom: 20px;
-  }
+.company-name {
+  font-weight: 600;
+  font-size: 20px;
+  margin-bottom: 20px;
+}
 
-  .salary-title {
-    margin-top: 50px;
-    font-size: 18px;
-    font-weight: 600;
-    text-align: center;
-  }
+.salary-title {
+  margin-top: 50px;
+  font-size: 18px;
+  font-weight: 600;
+  text-align: center;
+}
 
-  .payment-title,
-  .attendance-summary-title {
-    margin-top: 50px;
-    font-size: 18px;
-    font-weight: 600;
-  }
+.payment-title,
+.attendance-summary-title {
+  margin-top: 50px;
+  font-size: 18px;
+  font-weight: 600;
+}
 
-  .salary-details {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 20px;
-    border: 1px solid #ddd;
-    padding-left: 18px;
-    padding-right: 20px;
-  }
+.salary-details {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  border: 1px solid #ddd;
+  padding-left: 18px;
+  padding-right: 20px;
+}
 
-  .left-section,
-  .right-section {
-    width: 50%;
-  }
+.left-section,
+.right-section {
+  width: 50%;
+}
 
-  .right-section {
-    border-left: 1px solid #ddd;
-  }
+.right-section {
+  border-left: 1px solid #ddd;
+}
 
-  .right-section .row {
-    margin-left: 15px;
-  }
+.right-section .row {
+  margin-left: 15px;
+}
 
-  .row {
-    display: flex;
-    margin-bottom: 10px;
-  }
+.row {
+  display: flex;
+  margin-bottom: 10px;
+}
 
-  .label {
-    width: 40%;
-    font-size: 15px;
-    font-weight: 600;
-    color: #222;
-  }
+.label {
+  width: 40%;
+  font-size: 15px;
+  font-weight: 600;
+  color: #222;
+}
 
-  .value {
-    width: 60%;
-    font-size: 15px;
-  }
+.value {
+  width: 60%;
+  font-size: 15px;
+}
 
-  .salary-table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 20px;
-  }
+.salary-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
 
-  .salary-table th,
-  .salary-table td {
-    border: 1px solid #ddd;
-    padding: 8px;
-    padding-left: 15px;
-    text-align: left;
-    font-size: 15px;
-    margin-top: 10px;
-  }
+.salary-table th,
+.salary-table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  padding-left: 15px;
+  text-align: left;
+  font-size: 15px;
+  margin-top: 10px;
+}
 
-  .net-pay {
-    margin-top: 40px;
-    border: 1px solid #ddd;
-    padding: 10px;
-    padding-left: 15px;
-  }
+.net-pay {
+  margin-top: 40px;
+  border: 1px solid #ddd;
+  padding: 10px;
+  padding-left: 15px;
+}
 
-  .net-pay .value {
-    font-size: 15px;
-    font-weight: 600;
-    color: #222;
-  }
+.net-pay .value {
+  font-size: 15px;
+  font-weight: 600;
+  color: #222;
+}
 
-  .attendance-summary {
-    border: 1px solid #ddd;
-    margin-top: 15px;
-  }
+.attendance-summary {
+  border: 1px solid #ddd;
+  margin-top: 15px;
+}
 
-  .attendance-row {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 10px;
-    padding-left: 15px;
-    padding-right: 15px;
-  }
+.attendance-row {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
 
-  .attendance-column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.attendance-column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .attendance-title {
-    font-weight: 600;
-    color: #222;
-    font-size: 15px;
-    margin-top: 10px;
-  }
+.attendance-title {
+  font-weight: 600;
+  color: #222;
+  font-size: 15px;
+  margin-top: 10px;
+}
 
-  .attendance-data {
-    font-weight: normal;
-    margin-top: 10px;
-    font-size: 15px;
-  }
+.attendance-data {
+  font-weight: normal;
+  margin-top: 10px;
+  font-size: 15px
+}
 
-  .footer {
-    text-align: center;
-    margin-top: 30px;
-    font-size: 14px;
-    color: #333;
-  }
-            </style>
-          </head>
+.footer {
+  text-align: center;
+  margin-top: 30px;
+  font-size: 14px;
+  color: #333;
+}
+</style>
+</head>
 
-              <body>
-                <div class="page-wrapper">
-                  <div class="content">
-                    <div class="salary-slip">
-                      <img src="${office[0]?.logo}" class="logo-section" alt="logo" />
-                    </div>
-                    <div class="company-details">
-                      <h4 class="company-name">${office[0]?.name}</h4>
-                      <hr />
-                    </div>
+<body>
+  <div class="page-wrapper">
+    <div class="content">
+      <div class="salary-slip">
+        <img src="${office[0]?.logo}" class="logo-section" alt="logo" />
+        </div>
+        <div class="company-details">
+          <h4 class="company-name">${office[0]?.name}</h4>
+          <hr />
+        </div>
 
-                    <h6 class="salary-title">Salary Slip (January 2025)</h6>
-                    <div class="salary-details">
-                      <div class="left-section">
-                        <div class="row" style="margin-top: 8px;">
-                          <div class="label">Employee Name</div>
-                          <div class="value">John Doe</div>
-                        </div>
-                        <div class="row">
-                          <div class="label">Designation</div>
-                          <div class="value">Software Engineer</div>
-                        </div>
-                        <div class="row">
-                          <div class="label">Department</div>
-                          <div class="value">IT</div>
-                        </div>
-                        <div class="row">
-                          <div class="label">Date of Joining</div>
-                          <div class="value">01/01/2020</div>
-                        </div>
-                        <div class="row">
-                          <div class="label">Phone Number</div>
-                          <div class="value">123-456-7890</div>
-                        </div>
-                      </div>
+        <h6 class="salary-title">Salary Slip (January 2025)</h6>
+        <div class="salary-details">
+          <div class="left-section">
+            <div class="row" style="margin-top: 8px;">
+              <div class="label">Employee Name</div>
+              <div class="value">John Doe</div>
+            </div>
+            <div class="row">
+              <div class="label">Designation</div>
+              <div class="value">Software Engineer</div>
+            </div>
+            <div class="row">
+              <div class="label">Department</div>
+              <div class="value">IT</div>
+            </div>
+            <div class="row">
+              <div class="label">Date of Joining</div>
+              <div class="value">01/01/2020</div>
+            </div>
+            <div class="row">
+              <div class="label">Phone Number</div>
+              <div class="value">123-456-7890</div>
+            </div>
+          </div>
 
-                      <div class="right-section">
-                        <div class="row" style="margin-top: 8px;">
-                          <div class="label">Transaction Id</div>
-                          <div class="value">TX12345</div>
-                        </div>
-                        <div class="row">
-                          <div class="label">Employee ID</div>
-                          <div class="value">EMP12345</div>
-                        </div>
-                        <div class="row">
-                          <div class="label">Monthly Gross</div>
-                          <div class="value">₹50,000</div>
-                        </div>
-                      </div>
-                    </div>
+          <div class="right-section">
+            <div class="row" style="margin-top: 8px;">
+              <div class="label">Transaction Id</div>
+              <div class="value">TX12345</div>
+            </div>
+            <div class="row">
+              <div class="label">Employee ID</div>
+              <div class="value">EMP12345</div>
+            </div>
+            <div class="row">
+              <div class="label">Monthly Gross</div>
+              <div class="value">₹50,000</div>
+            </div>
+          </div>
+        </div>
 
-                    <h6 class="payment-title">Payment & Salary (January 2025)</h6>
-                    <table class="salary-table">
-                      <thead>
-                        <tr>
-                          <th>Earnings</th>
-                          <th>Amount</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>Salary</td>
-                          <td>₹50,000</td>
-                        </tr>
-                      </tbody>
-                      <tfoot>
-                        <tr>
-                          <th>Total Earnings</th>
-                          <th>₹50,000</th>
-                        </tr>
-                      </tfoot>
-                    </table>
+        <h6 class="payment-title">Payment & Salary (January 2025)</h6>
+        <table class="salary-table">
+          <thead>
+            <tr>
+              <th>Earnings</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Salary</td>
+              <td>₹50,000</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>Total Earnings</th>
+              <th>₹50,000</th>
+            </tr>
+          </tfoot>
+        </table>
 
-                    <div class="net-pay">
-                      <div class="row">
-                        <div class="label">Net Payable (Total Earnings)</div>
-                        <div class="value">₹50,000</div>
-                      </div>
-                      <div class="row">
-                        <div class="label">Amount in Words</div>
-                        <div class="value">Fifty Thousand</div>
-                      </div>
-                    </div>
+        <div class="net-pay">
+          <div class="row">
+            <div class="label">Net Payable (Total Earnings)</div>
+            <div class="value">₹50,000</div>
+          </div>
+          <div class="row">
+            <div class="label">Amount in Words</div>
+            <div class="value">Fifty Thousand</div>
+          </div>
+        </div>
 
-                    <h6 class="attendance-summary-title">Attendance Summary (January 2025)</h6>
-                    <div class="attendance-summary">
-                      <div class="attendance-row">
-                        <div class="attendance-column">
-                          <div class="attendance-title">Present</div>
-                          <div class="attendance-data">20</div>
-                        </div>
-                        <div class="attendance-column">
-                          <div class="attendance-title">Absent</div>
-                          <div class="attendance-data">2</div>
-                        </div>
-                        <div class="attendance-column">
-                          <div class="attendance-title">Leave</div>
-                          <div class="attendance-data">1</div>
-                        </div>
-                        <div class="attendance-column">
-                          <div class="attendance-title">Comp Off</div>
-                          <div class="attendance-data">1</div>
-                        </div>
-                        <div class="attendance-column">
-                          <div class="attendance-title">Weekly Off</div>
-                          <div class="attendance-data">4</div>
-                        </div>
-                        <div class="attendance-column">
-                          <div class="attendance-title">Holiday</div>
-                          <div class="attendance-data">2</div>
-                        </div>
-                      </div>
-                    </div>
-                    <p class="footer">This is a digitally generated document and does not require a signature or seal.</p>
-                  </div>
-                </div>
-              </div>
-            </body>
-    </html>`;
+        <h6 class="attendance-summary-title">Attendance Summary (January 2025)</h6>
+        <div class="attendance-summary">
+          <div class="attendance-row">
+            <div class="attendance-column">
+              <div class="attendance-title">Present</div>
+              <div class="attendance-data">20</div>
+            </div>
+            <div class="attendance-column">
+              <div class="attendance-title">Absent</div>
+              <div class="attendance-data">2</div>
+            </div>
+            <div class="attendance-column">
+              <div class="attendance-title">Leave</div>
+              <div class="attendance-data">1</div>
+            </div>
+            <div class="attendance-column">
+              <div class="attendance-title">Comp Off</div>
+              <div class="attendance-data">1</div>
+            </div>
+            <div class="attendance-column">
+              <div class="attendance-title">Weekly Off</div>
+              <div class="attendance-data">4</div>
+            </div>
+            <div class="attendance-column">
+              <div class="attendance-title">Holiday</div>
+              <div class="attendance-data">2</div>
+            </div>
+          </div>
+        </div>
+        <p class="footer">This is a digitally generated document and does not require a signature or seal.</p>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>`;
 
     const fileNameBase = "salary";
     const directory = RNFS.DownloadDirectoryPath;
